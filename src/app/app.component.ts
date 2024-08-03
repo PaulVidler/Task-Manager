6b7a7374e011f06e0c3d5f4df5fb71c56bcbf44b
Paul Vidler <reliabledesignsandprint@gmail.com>
2024-08-04 09:32:55 +1000

    New Task omponent, Refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,27 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({ // this is a decorator, it is a way for typescript to add metadata to a class
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  //userName: string = "";
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onselectUser(id: string) {
-    console.log(`User id: ${id} selected`);
+    console.log(`User name: ${ this.selectedUser?.name} selected`);
+    this.selectedUserId = id;
   }
   
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,21 @@
-import { Component, Input, Output,EventEmitter } from '@angular/core';
+import { Component, Input, Output,EventEmitter, output } from '@angular/core';
 // import { DUMMY_USERS } from '../dummy-users';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+// alias or type definition
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+// interface definition
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+};
 
 @Component({
   selector: 'app-user',
@@ -11,10 +25,18 @@ import { Component, Input, Output,EventEmitter } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
+
+
+  // this is the old way of declaring an output decorator
+  @Output() select = new EventEmitter<string>();
+  
+  // this is an alternative method of declaring the output decorator using signal
+  // it may not be used outside of very modern projects
+  //select = output<string>(); 
 
   // this is an alternative method of declaring the input decorator using signal
   // avatar = input.required<string>();
@@ -33,14 +55,14 @@ export class UserComponent {
   // }
 
   get imagePath(){
-    return './users/' + this.avatar; 
+    return './users/' + this.user.avatar; 
   }
 
   // this below uses signals and is more efficient because it only updates the value when it is needed, not on every change of the component
   //imagePath = computed(() => './users/' + this.avatar());
 
   onSelectUser() {
-    console.log(`User ${this.name} selected`);
-    this.select.emit(this.id);
+    console.log(`User ${this.user.name} selected`);
+    this.select.emit(this.user.id);
   }
 }
