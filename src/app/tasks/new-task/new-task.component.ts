0363f3dd2df152d2d715c68348c976665f613bcd
Paul Vidler <reliabledesignsandprint@gmail.com>
2024-08-24 13:56:18 +1000

    Service rejigging

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,7 +1,8 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, inject, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { type NewTaskData } from "../task/task.model";
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -11,23 +12,27 @@ import { type NewTaskData } from "../task/task.model";
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>(); // void just means no data emitted
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>(); // void just means no data emitted
   enteredTitle = '';
   enteredSummary = '';
   enteredDueDate = '';
 
+  private tasksService = inject(TasksService)
+
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit(){
-    this.add.emit(
+    this.tasksService.addTasks(
       {
       title: this.enteredTitle,
       summary: this.enteredSummary,
       date: this.enteredDueDate,
-    });
-  }
+    }, this.userId
+  );
+    this.close.emit();
+  };
 
 }
